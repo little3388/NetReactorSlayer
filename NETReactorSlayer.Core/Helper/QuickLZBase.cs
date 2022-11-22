3b9dba526660cca8c20249374b77b27f3c5ff0fb
SychicBoy <SychicBoy@outlook.com>
2022-11-22 06:45:20 +0330

    Add missing license notices to files.

--- NETReactorSlayer.Core/Helper/QuickLZBase.cs
+++ NETReactorSlayer.Core/Helper/QuickLZBase.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+    Copyright (C) 2021 CodeStrikers.org
+    This file is part of NETReactorSlayer.
+    NETReactorSlayer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    NETReactorSlayer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with NETReactorSlayer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 
 namespace NETReactorSlayer.Core.Helper
 {
@@ -6,7 +21,6 @@ namespace NETReactorSlayer.Core.Helper
     {
         protected static uint Read32(byte[] data, int index) => BitConverter.ToUInt32(data, index);
 
-        // Can't use Array.Copy() when data overlaps so here's one that works
         protected static void Copy(byte[] src, int srcIndex, byte[] dst, int dstIndex, int size)
         {
             for (var i = 0; i < size; i++)
